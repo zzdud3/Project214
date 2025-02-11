b3e983fe8187885ee6ac0d5bd07aaad5756fd310
zzdud3 <rehmanzaine9@gmail.com>
2025-02-11 02:22:29 -0500

    Update script.js

--- script.js
+++ script.js
@@ -7,6 +7,7 @@ function loadYouTubePlayer() {
 
 // Start the quiz
 function startQuiz() {
+    // Hide the welcome screen and show the first question
     document.getElementById('welcome-screen').classList.add('hidden');
     document.getElementById('question1').classList.remove('hidden');
     loadYouTubePlayer();
@@ -24,6 +25,7 @@ function checkAnswer(questionId, isCorrect) {
 // Proceed to the next screen in the correct path
 function proceedToNextScreen(questionId) {
     if (questionId === 'question1') {
+        // Hide question 1 and show Valentine question
         document.getElementById('question1').classList.add('hidden');
         document.getElementById('valentine').classList.remove('hidden');
     }
@@ -32,6 +34,7 @@ function proceedToNextScreen(questionId) {
 // Show the incorrect question path
 function showIncorrectQuestion(questionId) {
     if (questionId === 'question1') {
+        // Hide question 1 and show first incorrect question
         document.getElementById('question1').classList.add('hidden');
         document.getElementById('question-wrong1').classList.remove('hidden');
     }
@@ -42,16 +45,33 @@ function checkWrongAnswer(questionId, isCorrect) {
     if (isCorrect) {
         proceedToNextScreenFromWrongPath(questionId);
     } else {
-        restartQuiz();
+        // If still incorrect, show final incorrect screen
+        showFinalIncorrectScreen();
     }
 }
 
 // Proceed to the next screen after answering wrong questions
 function proceedToNextScreenFromWrongPath(questionId) {
     if (questionId === 'question-wrong1') {
+        // Hide first incorrect question and show second incorrect question
         document.getElementById('question-wrong1').classList.add('hidden');
         document.getElementById('question-wrong2').classList.remove('hidden');
+    } else if (questionId === 'question-wrong2') {
+        // Hide second incorrect question and show third incorrect question
+        document.getElementById('question-wrong2').classList.add('hidden');
+        document.getElementById('question-wrong3').classList.remove('hidden');
+    } else if (questionId === 'question-wrong3') {
+        // If it's the last incorrect question, show final incorrect screen
+        showFinalIncorrectScreen();
     }
+}
+
+// Show the final incorrect screen
+function showFinalIncorrectScreen() {
+    document.getElementById('question-wrong1').classList.add('hidden');
+    document.getElementById('question-wrong2').classList.add('hidden');
+    document.getElementById('question-wrong3').classList.add('hidden');
+    document.getElementById('incorrect-final').classList.remove('hidden');
 }
 
 // Go to the date selection after "Will you be my Valentine?"
